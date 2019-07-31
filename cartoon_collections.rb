ea386ed1f81e0258924613e767f113bf7236c7fc
Aaron Blum <blumaa@gmail.com>
2019-07-31 04:24:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    puts "#{index}
+  end
 end
 
 def summon_captain_planet# code an argument here
